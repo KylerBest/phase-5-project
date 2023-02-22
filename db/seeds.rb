119f0598b9cac2965fdc86836524f47ef4dae59b
Kyler Best <kyler.b@live.com>
2023-02-22 11:42:53 -0600

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Client.create(name: "Liam Smith", phone: "123-456-7890", address: "123 Main St, Anytown, USA", email: "liam.smith@example.com", password: "password", password_confirmation: "password")
+Plumber.create(name: "John Doe", manager: true, phone: "098-765-4321", email: "john.doe@example.com", password: "password", password_confirmation: "password")
+Job.create(client_id: 1, type_of_work: "Repair", description: "My kitchen sink ain't workin'")
+Client.create(name: "Jack Black", phone: "156-356-7690", address: "Jack Black's house", email: "jack.black@example.com", password: "password", password_confirmation: "password")
+Job.create(client_id: 3, status: "In progress", type_of_work: "Unclogging/Cleaning", description: "Yo, Jack Black here. I'm gonna need some guys to come unclog my toilet. I pulled a 'Kung Fu Panda' if you know what I'm saying.")
+Plumber.create(name: "Jesse Barker", phone: "512-423-4353", email: "jesse.barker@example.com", password: "password", password_confirmation: "password")
+Assignment.create(plumber_id: 4, job_id: 2)
+
